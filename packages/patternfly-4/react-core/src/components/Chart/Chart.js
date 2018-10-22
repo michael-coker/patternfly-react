73e92bec303e0cff5cb2c69636a619f5f34dbe4e
Michael Coker <mcoker@redhat.com>
2018-10-22 17:31:58 -0500

    add global vars, other updates

--- packages/patternfly-4/react-core/src/components/Chart/Chart.js
+++ packages/patternfly-4/react-core/src/components/Chart/Chart.js
@@ -11,11 +11,124 @@ import {
   VictoryStack,
   Bar,
   VictoryGroup,
-  assign,
   theme,
   VictoryTheme
 } from 'victory';
 import './chart.css';
+import {
+  global_Color_100,
+  global_Color_200,
+  global_BorderColor,
+  global_active_color_100,
+  global_primary_color_100,
+  global_link_Color,
+  global_link_Color_hover,
+  c_nav__tertiary_list_link_FontWeight,
+  global_BackgroundColor_100,
+  global_BackgroundColor_200,
+  global_BackgroundColor_300,
+  global_BackgroundColor_light_100,
+  global_BackgroundColor_light_200,
+  global_BackgroundColor_light_300,
+  global_BackgroundColor_dark_100,
+  global_BackgroundColor_dark_200,
+  global_BackgroundColor_dark_transparent_100,
+  global_BackgroundColor_dark_transparent_200,
+  global_Color_light_100,
+  global_Color_light_200,
+  global_Color_dark_100,
+  global_Color_dark_200,
+  global_active_color_200,
+  global_active_color_300,
+  global_disabled_color_100,
+  global_disabled_color_200,
+  global_primary_color_200,
+  global_primary_color_light_100,
+  global_primary_color_dark_100,
+  global_secondary_color_100,
+  global_success_color_100,
+  global_success_color_200,
+  global_info_color_100,
+  global_info_color_200,
+  global_warning_color_100,
+  global_warning_color_200,
+  global_danger_color_100,
+  global_danger_color_200,
+  global_danger_color_300,
+  global_BoxShadow_sm,
+  global_BoxShadow_sm_right,
+  global_BoxShadow_sm_left,
+  global_BoxShadow_sm_bottom,
+  global_BoxShadow_sm_top,
+  global_BoxShadow_md,
+  global_BoxShadow_md_right,
+  global_BoxShadow_md_left,
+  global_BoxShadow_md_bottom,
+  global_BoxShadow_md_top,
+  global_BoxShadow_lg,
+  global_BoxShadow_lg_right,
+  global_BoxShadow_lg_left,
+  global_BoxShadow_lg_bottom,
+  global_BoxShadow_lg_top,
+  global_BoxShadow_inset,
+  global_font_path,
+  global_spacer_xs,
+  global_spacer_sm,
+  global_spacer_md,
+  global_spacer_lg,
+  global_spacer_xl,
+  global_spacer_2xl,
+  global_spacer_3xl,
+  global_gutter,
+  global_ZIndex_xs,
+  global_ZIndex_sm,
+  global_ZIndex_md,
+  global_ZIndex_lg,
+  global_ZIndex_xl,
+  global_ZIndex_2xl,
+  global_breakpoint_xs,
+  global_breakpoint_sm,
+  global_breakpoint_md,
+  global_breakpoint_lg,
+  global_breakpoint_xl,
+  global_link_FontWeight,
+  global_link_TextDecoration,
+  global_link_Color_light_hover,
+  global_link_TextDecoration_hover,
+  global_BorderWidth_sm,
+  global_BorderWidth_md,
+  global_BorderWidth_lg,
+  global_BorderColor_dark,
+  global_BorderColor_light,
+  global_BorderColor_light_200,
+  global_BorderRadius_sm,
+  global_BorderRadius_lg,
+  global_FontFamily_sans_serif,
+  global_FontFamily_monospace,
+  global_FontSize_4xl,
+  global_FontSize_3xl,
+  global_FontSize_2xl,
+  global_FontSize_xl,
+  global_FontSize_lg,
+  global_FontSize_md,
+  global_FontSize_sm,
+  global_FontSize_xs,
+  global_FontWeight_light,
+  global_FontWeight_normal,
+  global_FontWeight_semi_bold,
+  global_FontWeight_bold,
+  global_LineHeight_sm,
+  global_LineHeight_md,
+  global_ListStyle,
+  global_Transition,
+  global_TimingFunction,
+  global_arrow_width,
+  global_arrow_width_lg,
+  global_target_size_MinWidth,
+  global_target_size_MinHeight
+} from '@patternfly/react-tokens';
+import { assign } from "lodash";
+
 
 const propTypes = {
   children: PropTypes.node,
@@ -31,7 +144,24 @@ const defaultProps = {
 // Start Theme //
 /***************/
 
-// Colors
+const global_active_color_100_var = global_active_color_100.var,
+      global_BorderColor_light_var = global_BorderColor_light.var,
+      global_FontSize_sm_var = global_FontSize_sm.var,
+      global_Color_dark_100_var = global_Color_dark_100.var,
+      global_Color_light_200_var = global_Color_light_200.var;
+
+// Labels
+const baseLabelStyles = {
+  fontFamily: global_FontFamily_sans_serif,
+  global_FontSize_sm_var,
+  padding: 3,
+  fill: global_Color_dark_100_var,
+  stroke: "transparent"
+};
+
+// *
+// * Colors
+// *
 const colors = [
   "#252525",
   "#525252",
@@ -41,16 +171,10 @@ const colors = [
   "#d9d9d9",
   "#f0f0f0"
 ];
+
 const charcoal = "#252525";
 const grey = "#969696";
 
-// Typography
-const sansSerif =
-  "'Gill Sans', 'Gill Sans MT', 'Ser­avek', 'Trebuchet MS', sans-serif";
-const letterSpacing = "normal";
-const fontSize = 14;
-
-// Layout
 const baseProps = {
   width: 450,
   height: 300,
@@ -58,23 +182,13 @@ const baseProps = {
   colorScale: colors
 };
 
-// Labels
-const baseLabelStyles = {
-  fontFamily: sansSerif,
-  fontSize,
-  letterSpacing,
-  padding: 10,
-  fill: charcoal,
-  stroke: "transparent"
-};
 const centeredLabelStyles = assign({ textAnchor: "middle" }, baseLabelStyles);
 
-// Strokes
 const strokeLinecap = "round";
 const strokeLinejoin = "round";
 
-// Put it all together...
 const fooTheme = {
+  /*
   area: assign({
     style: {
       data: {
@@ -83,11 +197,12 @@ const fooTheme = {
       labels: centeredLabelStyles
     }
   }, baseProps),
+  */
   axis: assign({
     style: {
       axis: {
         fill: "transparent",
-        stroke: charcoal,
+        stroke: global_BorderColor_light_var,
         strokeWidth: 1,
         strokeLinecap,
         strokeLinejoin
@@ -97,13 +212,13 @@ const fooTheme = {
       }),
       grid: {
         fill: "none",
-        stroke: "none",
+        stroke: global_Color_light_200_var,
         pointerEvents: "painted"
       },
       ticks: {
         fill: "transparent",
-        size: 1,
-        stroke: "transparent"
+        size: 8,
+        stroke: global_BorderColor_light_var
       },
       tickLabels: baseLabelStyles
     }
@@ -111,13 +226,14 @@ const fooTheme = {
   bar: assign({
     style: {
       data: {
-        fill: charcoal,
+        fill: global_active_color_100_var,
         padding: 8,
         strokeWidth: 0
       },
       labels: baseLabelStyles
     }
   }, baseProps),
+  /*
   boxplot: assign({
     style: {
       max: { padding: 8, stroke: charcoal, strokeWidth: 1 },
@@ -238,11 +354,12 @@ const fooTheme = {
       }
     }
   }, baseProps)
+  */
 };
 
 const Chart = ({ className, children, ...props }) => (
-  <VictoryChart theme={fooTheme}>
-    {/*    
+  <VictoryChart theme={fooTheme} domainPadding={30}>
+    {/*
     <VictoryBar
       data={sampleData}
       dataComponent={<Bar className="pf-c-chart__bar"/>}
@@ -253,24 +370,28 @@ const Chart = ({ className, children, ...props }) => (
     />
     */}
     <VictoryBar
-      data={sampleData}
-      dataComponent={<Bar className="pf-c-chart__bar-multi"/>}
+      data={[
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+        { x: 3, y: 5 },
+        { x: 4, y: 4 },
+        { x: 5, y: 6 }
+      ]}
     />
-    {/*
-    <VictoryGroup offset={20}
-      colorScale={"qualitative"}
+      {/*
+    <VictoryGroup offset={17}
     >
       <VictoryBar
-        data={[{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 5 }]}
-        dataComponent={<Bar className="pf-c-chart__bar-multi"/>}
+        data={[{ x: 2016, y: 1 }, { x: 2017, y: 2 }, { x: 2018, y: 5 }]}
       />
       <VictoryBar
-        data={[{ x: 1, y: 2 }, { x: 2, y: 1 }, { x: 3, y: 7 }]}
-        dataComponent={<Bar className="pf-c-chart__bar-multi"/>}
+        data={[{ x: 2016, y: 2 }, { x: 2017, y: 1 }, { x: 2018, y: 7 }]}
       />
       <VictoryBar
-        data={[{ x: 1, y: 3 }, { x: 2, y: 4 }, { x: 3, y: 9 }]}
-        dataComponent={<Bar className="pf-c-chart__bar-multi"/>}
+        data={[{ x: 2016, y: 3 }, { x: 2017, y: 4 }, { x: 2018, y: 9 }]}
+      />
+      <VictoryBar
+        data={[{ x: 2016, y: 3 }, { x: 2017, y: 4 }, { x: 2018, y: 10 }]}
       />
     </VictoryGroup>
     */}
